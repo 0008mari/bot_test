d6f088a35c33e842125f0d9c4d6afb5552eb44d0
0008mari <63359327+0008mari@users.noreply.github.com>
2020-06-11 15:11:24 +0900

    Add files via upload

--- thisbot.py
+++ thisbot.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-# tweepy-bots/bots/autoreply.py
 
 import tweepy
 import logging
 from config import create_api
 import random
-import os
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -14,15 +11,19 @@ logger = logging.getLogger()
 def writeTweetsfromTxt(api):
     logger.info("bot started")
 
-    f = open("C:\\Users\\00_ma\\AppData\\Local\\Programs\\Python\\Python38-32\\tweepy-bots\\bots\\data.txt", 'r', encoding='UTF-8') #파일 열기
+    f = open("C:\\Users\\00_ma\\AppData\\Local\\Programs\\Python\\Python38-32\\tweepy-bots\\bots\\tweet_file.txt", 'r', encoding='UTF-8') #파일 열기
     datas = f.readlines()
-    num = len(datas)+1
+    num = len(datas)+1 #각 줄을 리스트로 저장, num은 리스트 크기
+    # range 함수는 stop 숫자 미포함
 
-    ran =  random.randint(0, num-1)
+    ran =  random.randint(0, num-1) #난수 뽑기
+
+    result = datas[ran].replace("\\n", "\n")
     
-    #print(datas[ran])
+    print(result)
+    # 그 난수로 나온 애만 출력할때 바꿔준다.. 왜 다 복사하고 앉았너 
     
-    api.update_status(datas[ran])
+    api.update_status(result)
     
     f.close() #파일 닫기
     #os.system("pause")
